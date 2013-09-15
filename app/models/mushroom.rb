f5f03335dbe4f2b1ba9922eca571e892f8120dde
= <=>
2013-09-15 13:11:19 -0400

    Added growth_notes to attr_accessible params for mushroom model

--- app/models/mushroom.rb
+++ app/models/mushroom.rb
@@ -1,3 +1,3 @@
 class Mushroom < ActiveRecord::Base
-  attr_accessible :description, :difficulty, :fruit_time_max, :fruit_time_min, :latin_name, :price_per_ounce, :spawn_time_min, :spwan_time_max, :substrate, :variety
+  attr_accessible :description, :difficulty, :fruit_time_max, :fruit_time_min, :latin_name, :price_per_ounce, :spawn_time_min, :spwan_time_max, :substrate, :variety, :growth_notes
 end
