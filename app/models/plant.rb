dc9a29c3c7818c46d9ad7d3c959141db45f301b8
= <=>
2013-09-22 19:31:12 -0400

    changed plant index page to have tiled style layout

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,3 +1,9 @@
 class Plant < ActiveRecord::Base
   attr_accessible :variety, :latin_name, :description, :maxDaysToGerm, :maxDaysToMaturity, :minDaysToGerm, :minDaysToMaturity, :days_til_pollination
+  
+  has_attached_file :picture, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
 end
