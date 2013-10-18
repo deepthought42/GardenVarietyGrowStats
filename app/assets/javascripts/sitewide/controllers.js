264c973a8024bf0e027041219f0b7014503fd52e
= <=>
2013-10-17 23:18:12 -0400

    changed plants index template to use angular for rendering

--- app/assets/javascripts/sitewide/controllers.js
+++ app/assets/javascripts/sitewide/controllers.js
@@ -4,10 +4,17 @@ var gardenApp = angular.module('gardenApp', []);
 /* Controllers */
 
 function MushroomListCtrl($scope, $http) {
-
   $http.get('/mushrooms.json').success(function(data) {
     $scope.mushrooms = data;
   });
 }
 
 gardenApp.controller('MushroomListCtrl', ['$scope', '$http', MushroomListCtrl])
+
+function PlantListCtrl($scope, $http) {
+  $http.get('/plants.json').success(function(data) {
+    $scope.plants = data;
+  });
+}
+
+gardenApp.controller('PlantListCtrl', ['$scope', '$http', PlantListCtrl])
