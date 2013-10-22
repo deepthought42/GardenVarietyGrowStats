b84e53ced9c1a6394090f0ba70ccbf329802d0c0
= <=>
2013-10-22 00:13:55 -0400

    changed templateUrls for mushrooms

--- app/assets/javascripts/sitewide/app.js
+++ app/assets/javascripts/sitewide/app.js
@@ -5,7 +5,7 @@
 var gardenApp = angular.module('gardenApp', []).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/mushrooms', {templateUrl: 'partials/mushroom-list.html',   controller: 'MushroomListCtrl'}).
-      when('/mushrooms/:mushroomId', {templateUrl: 'partials/mushroom-detail.html', controller: 'MushroomDetailCtrl'}).
+      when('/mushrooms', {templateUrl: 'mushrooms/partials/mushroom-list.html',   controller: 'MushroomListCtrl'}).
+      when('/mushrooms/:mushroomId', {templateUrl: 'mushrooms/partials/mushroom-detail.html', controller: 'MushroomDetailCtrl'}).
       otherwise({redirectTo: '/mushrooms'});
 }]);
