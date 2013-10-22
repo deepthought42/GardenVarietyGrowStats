8c6d1fe8aa6dacb155d92a92bf15788a8fffa762
= <=>
2013-10-22 00:19:07 -0400

    Still trying to figure out routing

--- app/assets/javascripts/sitewide/app.js
+++ app/assets/javascripts/sitewide/app.js
@@ -5,7 +5,7 @@
 var gardenApp = angular.module('gardenApp', []).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/mushrooms', {templateUrl: 'mushrooms/partials/mushroom-list.html',   controller: 'MushroomListCtrl'}).
-      when('/mushrooms/:mushroomId', {templateUrl: 'mushrooms/partials/mushroom-detail.html', controller: 'MushroomDetailCtrl'}).
+      when('/mushrooms', {templateUrl: 'mushrooms/mushroom-list.html',   controller: 'MushroomListCtrl'}).
+      when('/mushrooms/:mushroomId', {templateUrl: 'mushrooms/mushroom-detail.html', controller: 'MushroomDetailCtrl'}).
       otherwise({redirectTo: '/mushrooms'});
 }]);
